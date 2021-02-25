c0c3a023bf2db6708f3f87298712416dc943f624
Vitor Henckel <vitor@henckel.com.br>
2021-02-24 22:08:47 -0300

    style: adjusting identation and styles

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controllers'
 export class SignUpController implements Controller {
-  handle(httpRequest: HttpRequest): HttpResponse {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
