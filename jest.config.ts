83051d941ac4486d9e3d4042fa16b9b760e0a2ce
Vitor Henckel <vitor@henckel.com.br>
2021-02-24 21:27:32 -0300

    refactor: add customized error for missing param

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
